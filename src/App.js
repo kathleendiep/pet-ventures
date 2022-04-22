744a403e8b3e6507370fe01e2c437f3793f625f1
kathleendiep <kathleendiep@gmail.com>
2022-04-21 21:51:27 -0700

    npm package

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/homePage/homePage';
-import ViewAll from './components/viewAll/viewAll';
 import About from './components/about/about';
 import LoaderPage from './components/loader/loaderpage'
 function App() {
@@ -19,7 +18,7 @@ function App() {
           <Route path="/" element={<LoaderPage />} />
           <Route path="/" element={<Navigate replace to="/" />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/viewall" element={<ViewAll />} />
+          {/* <Route path="/viewall" element={<ViewAll />} /> */}
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
