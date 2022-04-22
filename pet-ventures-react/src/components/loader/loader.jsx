d3d3ed06dd571980ac7f1c97323d7dd42c750d6b
kathleendiep <kathleendiep@gmail.com>
2022-04-21 22:46:40 -0700

    scroll in altered

--- pet-ventures-react/src/components/loader/loader.jsx
+++ pet-ventures-react/src/components/loader/loader.jsx
@@ -3,20 +3,29 @@ import "./loader.css";
 class Loader extends Component {
     render() {
         return (
-            // if loading is true, then show this, if not deactivate
-            <div className={`preloader ${this.props.loading ? '' : 'preloader-deactivate'}`}>
-                <div className="d-table">
-                    <div className="d-table-cell">
-                        <div className="spinner">
-                            <div className="rect1"></div>
-                            <div className="rect2"></div>
-                            <div className="rect3"></div>
-                            <div className="rect4"></div>
-                            <div className="rect5"></div>
-                        </div>
+            <>
+            {
+                this.props.loading
+                ? 
+                 // if loading is true, then show this, if not deactivate
+            <div className="preloader">
+            <div className="d-table">
+                <div className="d-table-cell">
+                    <div className="spinner">
+                        <div className="rect1"></div>
+                        <div className="rect2"></div>
+                        <div className="rect3"></div>
+                        <div className="rect4"></div>
+                        <div className="rect5"></div>
                     </div>
                 </div>
             </div>
+        </div>
+                :
+                    ""
+            }
+            </>
+           
         );
     }
 }
