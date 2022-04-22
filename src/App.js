6608fab3b7d2cb894ae21cf8267d26c14260777d
kathleendiep <kathleendiep@gmail.com>
2022-04-21 21:43:30 -0700

    added spinner

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/homePage/homePage';
 import ViewAll from './components/viewAll/viewAll';
 import About from './components/about/about';
+import LoaderPage from './components/loader/loaderpage'
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/viewall" element={<ViewAll />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/" element={<Navigate replace to="/home" />} />
-          </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoaderPage />} />
+          <Route path="/" element={<Navigate replace to="/" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/viewall" element={<ViewAll />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
