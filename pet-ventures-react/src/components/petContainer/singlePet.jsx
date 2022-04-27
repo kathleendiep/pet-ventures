c551cb9572fb5c049f7bee17e0308416b684adb7
kathleendiep <kathleendiep@gmail.com>
2022-04-27 10:17:46 -0700

    cards ready

--- pet-ventures-react/src/components/petContainer/singlePet.jsx
+++ pet-ventures-react/src/components/petContainer/singlePet.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 const SinglePet = (props) => {
-    return ( 
+  return ( 
         <>
-       <h1>{props.pet.name}</h1> 
-       <p>{props.pet.category}</p> 
-       <p>{props.pet.username}</p> 
-       </>
-     );
+      <div className="single-item-component">
+        <div class="cards">
+          <div class="card">
+            <div class="img">
+              <img className="image-voyager" src=></img>
+            </div>
+            <div class="card__body">
+              <div class="card__color-picker">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              <h2 className="card-title">{props.pet.name}</h2>
+              <h3 >{props.pet.category}</h3>
+              <li className="card-jobtitle">{props.pet.breed}</li>
+              <li>{props.pet.city}</li>
+              <div class="card-content">
+                <div class="card-subtitle">ABOUT</div>
+                <p class="card-desc">{props.pet.infos}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+      </>
+      );
 }
 
-export default SinglePet;
+      export default SinglePet;
